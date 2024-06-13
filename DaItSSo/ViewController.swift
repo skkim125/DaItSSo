dd5a08ab7e06cc724fd84c26eb20494285a1fb09
skkim125 <kthanks125@gmail.com>
2024-06-13 23:18:31 +0900

    Initial Commit

---
+++ DaItSSo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaItSSo
+//
+//  Created by 김상규 on 6/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
