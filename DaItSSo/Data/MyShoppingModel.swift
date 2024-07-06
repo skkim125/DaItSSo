c9dc766e9e6f615d4d46e9dba020d20756e53ea1
skkim125 <kthanks125@gmail.com>
2024-07-06 18:29:23 +0900

    DaItSSo - 나의 쇼핑 상품 데이터를 저장하기 위해 Realm 라이브러리 설치 - MyShoppingModel: Realm Database에 저장하기 위한 데이터 구조 설계 / 나의 쇼핑 저장 및 삭제 메서드 설계 및 임시로 MyShoppingModel 파일에 저장 - etc: UIViewController의 extension에 이미지 저장 관련 메서드 추가

---
+++ DaItSSo/Data/MyShoppingModel.swift
@@ -0,0 +1,52 @@
+//
+//  MyShopping.swift
+//  DaItSSo
+//
+//  Created by 김상규 on 7/6/24.
+//
+
+import Foundation
+import RealmSwift
+
+
+final class MyShoppingModel: Object {
+    @Persisted(primaryKey: true) var productId: String
+    @Persisted(indexed: true) var title: String
+    @Persisted var mallName: String
+    @Persisted var lprice: String
+    @Persisted var link: String
+    
+    convenience init(productId: String, title: String, mallName: String, lprice: String, link: String) {
+        self.init()
+        self.productId = productId
+        self.title = title
+        self.mallName = mallName
+        self.lprice = lprice
+        self.link = link
+    }
+
+}
+
+final class MyShoppingRepository {
+    private let realm = try! Realm()
+    
+    func addMyShopping(_ myShopping: MyShoppingModel) {
+        do {
+            try realm.write {
+                realm.add(myShopping)
+            }
+        } catch {
+            print("Realm Error")
+        }
+    }
+    
+    func deleteMyShopping(_ myShopping: MyShoppingModel) {
+        do {
+            try realm.write {
+                realm.delete(myShopping)
+            }
+        } catch {
+            print("Realm Error")
+        }
+    }
+}
